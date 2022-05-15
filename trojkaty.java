c5614ecee4055d9597491b64d92dd1dc0f5ed320
Krzysztof Wróbel <calwera@gmail.com>
2022-05-15 11:17:32 +0200

    Junior tester: pierwszy commit! :)

--- trojkaty.java
+++ trojkaty.java
@@ -30,7 +30,11 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
-        
+	
+	if ( a< 0 || b < 0 || c < 0) {
+		System.out.println("Długość boków trójkąta muszą być nieujemnne!");
+		System.exit(2);
+	}        
         jakiTrojkat(a, b, c);
     }
 }
